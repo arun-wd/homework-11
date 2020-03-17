7b0c7d6da447c7a4717b05006bcfc721e040bca2
RunS WD <anair8793@gmail.com>
2020-03-17 14:31:22 -0500

    added js files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const api = require("./source/api");
+const html = require("./source/html");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencode({ extended: true}));
+app.use(express.static("public"));
+app.use("/api", api);
+app.use("/", html);
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
---
+++ source/api.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+
+
+router.get("/notes", function(req,res){
+
+    
+});
+
+router.post("/notes", function(req,res){
+
+});
+
+router.delete("/notes/:id", function(req,res){
+
+});
+
+module.exports = router;
